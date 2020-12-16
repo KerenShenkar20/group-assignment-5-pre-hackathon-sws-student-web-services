637d5cdd6c45a856f4088c47fb2c40e879782ecc
Itamar Yarden <itamary9@gmail.com>
2020-12-16 19:13:26 +0200

    Itamar last update before work

--- service/server.js
+++ service/server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const {orderRouter, restaurantRouter, userRouter} = require('./router');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
