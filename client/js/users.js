4f8c73c2a3995d61a3094decd2a894c42cb1a452
Niv Swisa <nivswisa9@gmail.com>
2020-12-17 13:18:29 +0200

    buttify and changing button update NIV

--- client/js/users.js
+++ client/js/users.js
@@ -1,9 +1,35 @@
 
 const service_url = 'http://localhost:3000/api/users';
-let data = {};
 
+//Self invoke  for the button listeners
 userOperationsListeners();
 
+// logics of the buttons.
+function userOperationsListeners() {
+    //Update button, display the option to update user.
+    $("#update-button").click(() => {
+        $("#get-update-user").css("display", "block");
+        $("#get-update-do").text("Get");
+    });
+
+    $("#get-update-do").click(() => {
+        const userId = $("#user-id").val();
+        console.log(userId);
+
+        getUserById(userId);
+    });
+    //Show button, calling get all users function.
+    $("#show-button").click(() => {
+        getAllUsers();
+    });
+
+    $("#Hide-button").click(() => {
+        $("table").hide();
+    });
+}
+
+
+//CRUD functions, GET & UPDATE
 function getAllUsers() {
     $.ajax({
         url: service_url,
@@ -25,7 +51,6 @@ function getUserById(userId) {
 }
 
 function updateUserById(userId, info) {
-
     $.ajax({
         url: service_url + `/${userId}`,
         type: 'PUT',
@@ -36,29 +61,23 @@ function updateUserById(userId, info) {
 function findUser(user) {
     $("#user-result").empty();
     $("#user-result").append(
-        `First Name <input type="text" placeholder="${user.first_name}" id="firstName">` +
-        '<button type="button" class="btn-danger" id="updateFisrtName">Enter</button><br>' +
-        `Last Name <input type="text" placeholder="${user.last_name}" id="lastName">` +
-        '<button type="button" class="btn-danger" id="updateLastName">Enter</button><br>' +
-        `Email <input type="text" placeholder="${user.email}">` +
-        '<button type="button" class="btn-danger" id="updateEmail">Enter</button><br>' +
-        `Gender <input type="text" placeholder="${user.gender}">` +
-        '<button type="button" class="btn-danger" id="updateGender">Enter</button><br>'
+        `<input type="text" placeholder="key" id="key">` +
+        `<input type="text" placeholder="value" id="value">` +
+        '<button type="button" class="btn-danger" id="updateFisrtName">Enter</button><br>'
     );
 
         $("#updateFisrtName").click(() => {
-            const firstName = document.getElementById("firstName").value;
-            const lastName = document.getElementById("lastName").value;
-            
-            data = {"first_name" : firstName};
+            let data = {};
+            const key = document.getElementById("key").value;
+            const value = document.getElementById("value").value;
+            data[key] = value;
+            console.log(data);
             updateUserById(user.id, data);
         });
-
 }
 
-
+// creates a table with all the users, by the following format - all users with all the information about them, ignore the color column - just color the font of the row and show the avatar in the right column.
 function recreateUsersTable(users) {
-
     // EXTRACT VALUE FOR HTML HEADER. 
     let col = [];
     for (let i = 0; i < users.length; i++) {
@@ -68,73 +87,44 @@ function recreateUsersTable(users) {
             }
         }
     }
-
     // CREATE DYNAMIC TABLE.
     let table = document.createElement("table");
-
     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-    let tr = table.insertRow(-1);                   // TABLE ROW.
+    let tr = table.insertRow(-1);// TABLE ROW.
 
     for (let i = 0; i < col.length; i++) {
-
         if (i == 6)
             continue;
-        let th = document.createElement("th");      // TABLE HEADER.
+        let th = document.createElement("th");// TABLE HEADER.
         th.innerHTML = col[i];
         tr.appendChild(th);
     }
 
     // ADD JSON DATA TO THE TABLE AS ROWS.
     for (let i = 0; i < users.length; i++) {
-
         tr = table.insertRow(-1);
+        //each user gets colored font accordingly to his color, hides the col of the color.
         tr.style.color = users[i][col[7]];
         for (let j = 0; j < col.length; j++) {
-
             if (j == 5)
                 continue;
 
             let tabCell = tr.insertCell(-1);
             if (j == 6) {
-
+                //Showing avatar as backgroundimage in the table cell
                 tabCell.style.backgroundImage = `url(${users[i][col[j]]})`
                 continue;
             }
-
             tabCell.innerHTML = users[i][col[j]];
         }
     }
-
     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
     const divContainer = document.getElementById("showData");
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
 }
 
-function userOperationsListeners() {
-    $("#update-button").click(() => {
-        $("#get-delete-user").css("display", "block");
-        $("#get-delete-do").text("Get");
-    });
 
-    $("#get-delete-do").click(() => {
-        const userId = $("#user-id").val();
-        console.log(userId);
-
-        getUserById(userId);
-    });
-
-    $("#show-button").click(() => {
-        getAllUsers();
-    });
-
-
-    $("#Hide-button").click(() => {
-        $("table").hide();
-    });
-
-}
 
 
 
